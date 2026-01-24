9d9cb900b1c84673adfb3453ec13fd39f7503e77
thekim123 <tacdo12@naver.com>
2026-01-24 11:20:14 +0900

    initial commit

---
+++ src/main/java/com/thekim123/jobclient/JobClientApplication.java
@@ -0,0 +1,13 @@
+package com.thekim123.jobclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thekim123/jobclient/JobClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thekim123.jobclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
